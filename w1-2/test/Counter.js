e8ed295c2998ffdb43dd7603a442a22b75ae997e
GarenWoo <126687110+GarenWoo@users.noreply.github.com>
2023-03-13 10:58:16 +0800

    Update Counter.js

--- w1-2/test/Counter.js
+++ w1-2/test/Counter.js
@@ -19,35 +19,23 @@ describe("Counter", function () {
     await init();
   });
 
-  // 
-//   it("init equal 0", async function () {
-//     expect(await counter.counter()).to.equal(0);
-//   });
-
-//   it("add 1 equal 1", async function () {
-//     let tx = await counter.count();
-//     await tx.wait();
-//     expect(await counter.counter()).to.equal(1);
-//   });
-
-  it("owner init equal 0", async function () {
+  it("owner init equal 0", async function () {                //check the initial value of counter is 0 as expected
     expect(await counter.connect(owner).counter()).to.equal(0);
   });
 
-  it("owner add 1 equal 1", async function () {
+  it("owner add 1 equal 1", async function () {          
     let tx = await counter.count();
     await tx.wait();
     //console.log(tx);
     expect(await counter.connect(owner).counter()).to.equal(1);
   });
   it("otherAccount add equal 1", async function () {
-    //捕获异常
     try{
         let tx = await counter.connect(otherAccount).count();
         await tx.wait();
         //console.log(tx);
     }catch {
-        console.log("otherAccount add equal 1 failed");
+        console.log("Only owner can use count()");
     }
  
     expect(await counter.connect(otherAccount).counter()).to.equal(1);
